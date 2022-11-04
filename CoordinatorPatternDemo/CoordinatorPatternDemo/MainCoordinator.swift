161cc3a121800ea551185f14afb3a5f8a7ca787c
PatrickR <patrick@ruzman.net>
2022-11-04 10:59:33 +0100

    CoordinatorPattern Advanced [Change] back navigation from viewDidDisappear to using navigationController delegate in main coordinator

--- CoordinatorPatternDemo/CoordinatorPatternDemo/MainCoordinator.swift
+++ CoordinatorPatternDemo/CoordinatorPatternDemo/MainCoordinator.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 
-class MainCoordinator: Coordinator {
+class MainCoordinator: NSObject, Coordinator, UINavigationControllerDelegate {
     var childCoordinators = [Coordinator]()
     var navigationController: UINavigationController
 
@@ -17,6 +17,7 @@ class MainCoordinator: Coordinator {
     }
 
     func start() {
+        navigationController.delegate = self
         let vc = ViewController.instantiate()
         vc.coordinator = self
         navigationController.pushViewController(vc, animated: false)
@@ -41,6 +42,14 @@ class MainCoordinator: Coordinator {
                 childCoordinators.remove(at: index)
                 break
             }
+        }
+    }
+
+    func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
+        guard let fromViewController = navigationController.transitionCoordinator?.viewController(forKey: .from) else {return}
+        if navigationController.viewControllers.contains(fromViewController) {return}
+        if let buyViewController = fromViewController as? BuyViewController {
+            childDidFinish(buyViewController.coordinator)
         }
     }
 }
