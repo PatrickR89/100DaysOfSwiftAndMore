cc34eceea685a44f0e1503a966020f68cd47cb1d
PatrickR <patrick@ruzman.net>
2022-11-04 10:08:31 +0100

    CoordinatorPattern [Add] MainCoordinator class
    - [Add] create class in scene delegate (opposed to app delegate in hackingwithswift tutorial)
    - [Add] programatic creation of UI screen

---
+++ CoordinatorPatternDemo/CoordinatorPatternDemo/MainCoordinator.swift
@@ -0,0 +1,23 @@
+//
+//  MainCoordinator.swift
+//  CoordinatorPatternDemo
+//
+//  Created by Patrick on 04.11.2022..
+//
+
+import Foundation
+import UIKit
+
+class MainCoordinator: Coordinator {
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func start() {
+        let vc = ViewController.instantiate()
+        navigationController.pushViewController(vc, animated: false)
+    }
+}
