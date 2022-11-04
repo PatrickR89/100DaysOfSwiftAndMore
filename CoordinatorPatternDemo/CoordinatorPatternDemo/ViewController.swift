376623d082ea162945991f95777d2398b8d86484
PatrickR <patrick@ruzman.net>
2022-11-04 10:20:28 +0100

    Coordinator Pattern [Add] additional view controllers
    - [Add] tie viewControllers to coordinator
    - [Add] functions to mainCoordinator to push viewControllers

--- CoordinatorPatternDemo/CoordinatorPatternDemo/ViewController.swift
+++ CoordinatorPatternDemo/CoordinatorPatternDemo/ViewController.swift
@@ -9,12 +9,18 @@ import UIKit
 
 class ViewController: UIViewController, Storyboarded {
 
+    weak var coordinator: MainCoordinator?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .blue
     }
-
+    @IBAction func buyTapped(_ sender: Any) {
+    }
+    @IBAction func createAccTapped(_ sender: Any) {
+    }
+    
 
 }
 
