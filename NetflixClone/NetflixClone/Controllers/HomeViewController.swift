9a2359dacfef3182494b89c4a358ddcb6b74a201
PatrickR <patrick@ruzman.net>
2022-11-17 13:02:18 +0100

    NC -[Add] capitalize first letter section headers

--- NetflixClone/NetflixClone/Controllers/HomeViewController.swift
+++ NetflixClone/NetflixClone/Controllers/HomeViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
-    let sectionTitles: [String] = ["Trending movies", "Popular", "Trending TV", "Upcoming movies", "Top rated"]
+    let sectionTitles: [String] = ["Trending movies", "Trending TV", "Popular", "Upcoming movies", "Top rated"]
     private let homeFeedTable: UITableView = {
         let table = UITableView(frame: .zero, style: .grouped)
         table.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
@@ -101,7 +101,7 @@ extension HomeViewController: UITableViewDataSource {
         header.textLabel?.font = .systemFont(ofSize: 18, weight: .semibold)
         header.textLabel?.frame = CGRect(x: header.bounds.origin.x + 20, y: header.bounds.origin.y, width: 100, height: header.bounds.height)
         header.textLabel?.textColor = .white
-        header.textLabel?.text = header.textLabel?.text?.lowercased()
+        header.textLabel?.text = header.textLabel?.text?.capitalizeFirstLetter()
 
     }
 }
