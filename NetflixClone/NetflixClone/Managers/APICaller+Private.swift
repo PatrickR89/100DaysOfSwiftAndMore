c900ca0bda9e3c368c787c371392256bd509dc7d
PatrickR <patrick@ruzman.net>
2022-11-24 17:58:38 +0100

    NC - [Add] private struct with required data (private keys removed)

---
+++ NetflixClone/NetflixClone/Managers/APICaller+Private.swift
@@ -0,0 +1,15 @@
+//
+//  APICaller+Private.swift
+//  NetflixClone
+//
+//  Created by Patrick on 21.11.2022..
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = ""
+    static let baseURL = "https://api.themoviedb.org/"
+    static let YOUTUBE_API_KEY = ""
+    static let youtubeBaseUrl = "https://youtube.googleapis.com/youtube/v3/search?"
+}
