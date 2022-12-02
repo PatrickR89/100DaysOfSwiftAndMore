ec0b460c6c104d6e9c8c11b2372a9487e63fa70e
PatrickR <patrick@ruzman.net>
2022-12-02 12:05:22 +0100

    LeetCode - Move Zeroes (moving by one and swapping positions for non 0s)
    Runtime: 166 ms, faster than 91.82% of Swift online submissions for Move Zeroes.
    Memory Usage: 15 MB, less than 81.66% of Swift online submissions for Move Zeroes.

--- Playgrounds/MoveZeroes.playground/Contents.swift
+++ Playgrounds/MoveZeroes.playground/Contents.swift
@@ -1,21 +1,17 @@
 import UIKit
 
 func moveZeroes(_ nums: inout [Int]) {
-    var counter = 0
+    var pointer = 0
 
-    nums.forEach {
-        if $0 == 0 {
-            counter += 1
-        }
-    }
-
-    for _ in 0...counter {
-        if let firstIndex = nums.firstIndex(of: 0) {
-            nums.append(nums.remove(at: firstIndex))
+    for index in nums.indices {
+        if nums[index] != 0 {
+            let tempNum = nums[index]
+            nums[index] = nums[pointer]
+            nums[pointer] = tempNum
+            pointer += 1
         }
     }
 }
-
 var nums = [0,1,0,3,12]
 var nums2 = [0]
 moveZeroes(&nums)
