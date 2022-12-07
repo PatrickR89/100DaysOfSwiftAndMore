7432a75acbe2c47ca62c7d3dcccdc9216a07f838
PatrickR <patrick@ruzman.net>
2022-12-07 13:33:18 +0100

    LeetCode - Intersection of Two Arrays (better runtime, worse memory complexity)
    Runtime 54 ms Beats 11.6%
    Memory 14.4 MB Beats 49.79%

--- Playgrounds/IntersectionOfTwoArrays.playground/Contents.swift
+++ Playgrounds/IntersectionOfTwoArrays.playground/Contents.swift
@@ -2,19 +2,27 @@ import UIKit
 
 func intersection(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
 
-    var numArr = nums2
+//    var numArr = nums2
     var result = [Int]()
 
-    for index in 0..<nums1.count {
-        if let indexTwo = numArr.firstIndex(where: {$0 == nums1[index]}) {
-            if !result.contains(nums1[index]) {
-                result.append(nums1[index])
-            }
-            numArr.remove(at: indexTwo)
+//    for index in 0..<nums1.count {
+//        if let indexTwo = numArr.firstIndex(where: {$0 == nums1[index]}) {
+//            if !result.contains(nums1[index]) {
+//                result.append(nums1[index])
+//            }
+//            numArr.remove(at: indexTwo)
+//        }
+//    }
+
+    for num in nums1 {
+        if nums2.contains(num) && !result.contains(num) {
+            result.append(num)
         }
     }
 
     return result
+
+//    return Set<Int>(nums1.filter { nums2.contains($0) }).map { $0 }
 }
 
 intersection([1,2,2,1], [2,2])
