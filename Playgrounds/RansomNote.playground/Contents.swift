b9ce5d6fcc837e452e855560844d17dad8db5853
PatrickR <patrick@ruzman.net>
2022-12-08 12:43:29 +0100

    LeetCode - 383. Ransom Note (reduce memory complexity, avoid saving both strings as arrays, drastically reduce runtime)
    Runtime 42 ms Beats 81.88%
    Memory 14.3 MB Beats 79.10%

--- Playgrounds/RansomNote.playground/Contents.swift
+++ Playgrounds/RansomNote.playground/Contents.swift
@@ -2,21 +2,26 @@ import UIKit
 
 func canConstruct(_ ransomNote: String, _ magazine: String) -> Bool {
 
-    var ransomeNote = Array(ransomNote)
     var magazine = Array(magazine)
 
-    ransomeNote = ransomeNote.map {
-        if let index = magazine.firstIndex(of: $0) {
+//    ransomeNote = ransomeNote.map {
+//        if let index = magazine.firstIndex(of: $0) {
+//            magazine.remove(at: index)
+//            return "-"
+//        } else {
+//            return $0
+//        }
+//    }
+
+    for char in ransomNote {
+        if let index = magazine.firstIndex(of: char) {
             magazine.remove(at: index)
-            return "-"
         } else {
-            return $0
+            return false
         }
     }
 
-    print(ransomeNote.filter({$0 != "-"}).count)
-
-    return ransomeNote.filter({$0 != "-"}).count == 0
+    return true
 }
 
 canConstruct("a", "b")
