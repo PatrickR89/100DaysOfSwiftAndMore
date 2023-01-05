7a4f4fe6a06e85ee3c19bde5e37d1a0443128daf
PatrickR <patrick@ruzman.net>
2023-01-05 08:54:22 +0100

    LeetCode -  594. Longest Harmonious Subsequence (loop over dictionary - much faster)
    Runtime 794 ms Beats 38.46%
    Memory 14.7 MB Beats 30.77%

--- Playgrounds/LongestHarmoniousSubsequence.playground/Contents.swift
+++ Playgrounds/LongestHarmoniousSubsequence.playground/Contents.swift
@@ -2,50 +2,68 @@ import UIKit
 
 func findLHS(_ nums: [Int]) -> Int {
 
-    var lastNums = [Int]()
-    var currentNums = [Int]()
-    collectHarmonious(nums, 0, &lastNums, &currentNums)
+    var numsByValue = [Int: Int]()
+    for num in nums {
+        numsByValue[num, default: 0] += 1
+    }
 
-    print(lastNums)
+    var result = 0
 
-    return lastNums.count
-}
-
-func collectHarmonious(_ nums: [Int], _ index: Int, _ lastNums: inout [Int], _ currentNums: inout [Int]) {
-
-    currentNums.append(nums[index])
-
-    for numIndex in (index + 1)..<nums.count {
-        if nums[numIndex] == nums[index] || nums[numIndex] - nums[index] == 1 {
-            currentNums.append(nums[numIndex])
+    for num in numsByValue.keys {
+        if let greater = numsByValue[num + 1] {
+            result = max(numsByValue[num, default: 0] + greater, result)
         }
     }
 
-    compareResults(&lastNums, &currentNums)
-
-    currentNums.append(nums[index])
-
-    for numIndex in (index + 1)..<nums.count {
-        if nums[numIndex] == nums[index] || nums[numIndex] - nums[index] == -1 {
-            currentNums.append(nums[numIndex])
-        }
-    }
-
-    compareResults(&lastNums, &currentNums)
-
-    if index + 1 < nums.count {
-        collectHarmonious(nums, index + 1, &lastNums, &currentNums)
-    }
-
+    return result
 }
 
-func compareResults(_ lastNums: inout [Int], _ currentNums: inout [Int]) {
-    if currentNums.count > lastNums.count && currentNums.contains(where: { $0 != currentNums[0] }) {
-        lastNums = currentNums
-    }
+//func findLHS(_ nums: [Int]) -> Int {
+//
+//    var lastNums = [Int]()
+//    var currentNums = [Int]()
+//    collectHarmonious(nums, 0, &lastNums, &currentNums)
+//
+//    print(lastNums)
+//
+//    return lastNums.count
+//}
 
-    currentNums = []
-}
+//func collectHarmonious(_ nums: [Int], _ index: Int, _ lastNums: inout [Int], _ currentNums: inout [Int]) {
+//
+//    currentNums.append(nums[index])
+//
+//    for numIndex in (index + 1)..<nums.count {
+//        if nums[numIndex] == nums[index] || nums[numIndex] - nums[index] == 1 {
+//            currentNums.append(nums[numIndex])
+//        }
+//    }
+//
+//    compareResults(&lastNums, &currentNums)
+//
+//    currentNums.append(nums[index])
+//
+//    for numIndex in (index + 1)..<nums.count {
+//        if nums[numIndex] == nums[index] || nums[numIndex] - nums[index] == -1 {
+//            currentNums.append(nums[numIndex])
+//        }
+//    }
+//
+//    compareResults(&lastNums, &currentNums)
+//
+//    if index + 1 < nums.count {
+//        collectHarmonious(nums, index + 1, &lastNums, &currentNums)
+//    }
+//
+//}
+//
+//func compareResults(_ lastNums: inout [Int], _ currentNums: inout [Int]) {
+//    if currentNums.count > lastNums.count && currentNums.contains(where: { $0 != currentNums[0] }) {
+//        lastNums = currentNums
+//    }
+//
+//    currentNums = []
+//}
 
 findLHS([1,3,2,2,5,2,3,7])
 findLHS([1,2,3,4])
