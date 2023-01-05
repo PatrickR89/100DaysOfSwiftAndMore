8d3cd20c9684e5f61d06266459ae176aeb861193
PatrickR <patrick@ruzman.net>
2023-01-05 12:31:57 +0100

    LeetCode -  599. Minimum Index Sum of Two Lists (small changes)
    Runtime 957 ms Beats 10.87%
    Memory 14.6 MB Beats 89.13%

--- Playgrounds/MinimumIndexSumOfLists.playground/Contents.swift
+++ Playgrounds/MinimumIndexSumOfLists.playground/Contents.swift
@@ -3,21 +3,22 @@ import UIKit
 func findRestaurant(_ list1: [String], _ list2: [String]) -> [String] {
 
     var indexRestaurants = [String:Int]()
-
+    var minIndex = -1
     for index in list1.indices {
         if let secondIndex = list2.firstIndex(of: list1[index]) {
             indexRestaurants[list1[index]] = index + secondIndex
+            if index + secondIndex < minIndex || minIndex == -1 {
+                minIndex = index + secondIndex
+            }
         }
     }
 
-    let minValue = indexRestaurants.values.reduce(into: list1.count + list2.count) { result, index in
-        index < result ? (result = index) : (result = result)
-    }
+    var result = [String]()
 
-    let result = indexRestaurants.filter {
-        $0.value == minValue
-    }.map {
-        return $0.key
+    indexRestaurants.forEach {
+        if $0.value == minIndex {
+            result.append($0.key)
+        }
     }
 
     return result
