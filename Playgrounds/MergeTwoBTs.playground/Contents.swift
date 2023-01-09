3741dc222e3b222c42d0ca3154835c82dee2c436
PatrickR <patrick@ruzman.net>
2023-01-09 09:33:45 +0100

    LeetCode - 617. Merge Two Binary Trees
    Runtime 91 ms Beats 93.88%
    Memory 14.3 MB Beats 85.71%

---
+++ Playgrounds/MergeTwoBTs.playground/Contents.swift
@@ -0,0 +1,95 @@
+import UIKit
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+public class RootNode {
+    var rootNode: TreeNode?
+
+    init(_ array: [Int?]) {
+        self.rootNode = insertLevel(insertNils(array: array), 0)
+    }
+
+    func insertLevel(_ array: [Int?], _ index: Int) -> TreeNode? {
+
+        var root: TreeNode? = nil
+        if index < array.count {
+            guard let value = array[index] else {
+                return nil
+            }
+            root = TreeNode(value, nil, nil)
+
+            root?.left = insertLevel(array, 2 * index + 1)
+            root?.right = insertLevel(array, 2 * index + 2)
+        }
+        return root
+    }
+
+    func insertNils(array: [Int?]) -> [Int?] {
+        var array = array
+        var tempIndex = [Int]()
+
+        for (index, digit) in array.enumerated() {
+            if digit == nil {
+                if 2 * index + 1 < array.count {
+                    array.insert(nil, at: 2 * index + 1)
+                    array.insert(nil, at: 2 * index + 1)
+                    tempIndex.append(2 * index + 1)
+                    tempIndex.append(2 * index + 2)
+                }
+            }
+        }
+        if !tempIndex.isEmpty {
+            while 2 * tempIndex[0] + 2 < array.count {
+                for index in tempIndex {
+                    if 2*index+2 < array.count {
+                        array.insert(nil, at: 2 * index + 1)
+                        array.insert(nil, at: 2 * index + 1)
+                        tempIndex.append(2 * index + 1)
+                        tempIndex.append(2 * index + 2)
+                        tempIndex.sort()
+                    }
+
+                    let indOfIndex = tempIndex.firstIndex(where: {$0 == index})
+                    tempIndex.remove(at: indOfIndex!)
+                }
+            }
+        }
+        return array
+    }
+}
+
+func mergeTrees(_ root1: TreeNode?, _ root2: TreeNode?) -> TreeNode? {
+
+    return traverseWithMerge(root1, root2)
+}
+
+func traverseWithMerge(_ nodeOne: TreeNode?, _ nodeTwo: TreeNode?) -> TreeNode? {
+    guard let nodeOne = nodeOne else {
+        return nodeTwo
+    }
+
+    guard let nodeTwo = nodeTwo else {
+        return nodeOne
+    }
+
+    let left = traverseWithMerge(nodeOne.left, nodeTwo.left)
+    let right = traverseWithMerge(nodeOne.right, nodeTwo.right)
+
+    return TreeNode(nodeOne.val + nodeTwo.val, left, right)
+}
+
+let node10 = RootNode([1,3,2,5]).rootNode
+let node20 = RootNode([2,1,3,nil,4,nil,7]).rootNode
+
+mergeTrees(node10, node20)
