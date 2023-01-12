2908f549658c3b5f7ef97046e525670995571bac
PatrickR <patrick@ruzman.net>
2023-01-12 09:52:25 +0100

    LeetCode - 671. Second Minimum Node In a Binary Tree
    Runtime 0 ms Beats 100%
    Memory 14.3 MB Beats 26.32%

---
+++ Playgrounds/SecondMinNodeInBT.playground/Contents.swift
@@ -0,0 +1,100 @@
+import UIKit
+
+import UIKit
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+public class RootNode {
+    var rootNode: TreeNode?
+
+    init(_ array: [Int?]) {
+        self.rootNode = insertLevel(insertNils(array: array), 0)
+    }
+
+    func insertLevel(_ array: [Int?], _ index: Int) -> TreeNode? {
+
+        var root: TreeNode? = nil
+        if index < array.count {
+            guard let value = array[index] else {
+                return nil
+            }
+            root = TreeNode(value, nil, nil)
+
+            root?.left = insertLevel(array, 2 * index + 1)
+            root?.right = insertLevel(array, 2 * index + 2)
+        }
+        return root
+    }
+
+    func insertNils(array: [Int?]) -> [Int?] {
+        var array = array
+        var tempIndex = [Int]()
+
+        for (index, digit) in array.enumerated() {
+            if digit == nil {
+                if 2 * index + 1 < array.count {
+                    array.insert(nil, at: 2 * index + 1)
+                    array.insert(nil, at: 2 * index + 1)
+                    tempIndex.append(2 * index + 1)
+                    tempIndex.append(2 * index + 2)
+                }
+            }
+        }
+        if !tempIndex.isEmpty {
+            while 2 * tempIndex[0] + 2 < array.count {
+                for index in tempIndex {
+                    if 2*index+2 < array.count {
+                        array.insert(nil, at: 2 * index + 1)
+                        array.insert(nil, at: 2 * index + 1)
+                        tempIndex.append(2 * index + 1)
+                        tempIndex.append(2 * index + 2)
+                        tempIndex.sort()
+                    }
+
+                    let indOfIndex = tempIndex.firstIndex(where: {$0 == index})
+                    tempIndex.remove(at: indOfIndex!)
+                }
+            }
+        }
+        return array
+    }
+}
+
+func findSecondMinimumValue(_ root: TreeNode?) -> Int {
+
+    var minValues = [Int]()
+
+    searchValues(root, &minValues)
+    minValues.sort()
+    print(minValues)
+    return minValues.count > 1 ? minValues[1] : -1
+}
+
+func searchValues(_ node: TreeNode?, _ minValues: inout [Int]) {
+    if node == nil {
+        return
+    }
+
+    if !minValues.contains(node!.val) {
+        minValues.append(node!.val)
+    }
+
+    searchValues(node!.left, &minValues)
+    searchValues(node!.right, &minValues)
+
+}
+
+findSecondMinimumValue(RootNode([2,2,5,nil,nil,5,7]).rootNode)
+findSecondMinimumValue(RootNode([2,2,2]).rootNode)
+findSecondMinimumValue(RootNode([1,1,3,1,1,3,4,3,1,1,1,3,8,4,8,3,3,1,6,2,1]).rootNode)
