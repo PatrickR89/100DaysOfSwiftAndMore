5c6d881df7328beffc82ce85a2c9f710abfae08f
PatrickR <patrick@ruzman.net>
2023-01-13 10:09:29 +0100

    LeetCode - 680. Valid Palindrome II (a little bit better solution)
    Runtime 119 ms Beats 54.39%
    Memory 15.6 MB Beats 22.81%

--- Playgrounds/ValidPalindromeII.playground/Contents.swift
+++ Playgrounds/ValidPalindromeII.playground/Contents.swift
@@ -3,41 +3,33 @@ import UIKit
 func validPalindrome(_ s: String) -> Bool {
 
     let s = Array(s)
-
-
-
-    return checkSubsequence(s)
-}
-
-func checkSubsequence(_ s: [Character]) -> Bool {
-    if s.count == 1 {
-        return true
-    }
-
-    if s == s.reversed() {
-        return true
-    }
-
     var left = 0
     var right = s.count - 1
-    var charDeleted = false
-    while left <= right {
+
+    while left < right {
         if s[left] == s[right] {
             left += 1
             right -= 1
-        } else if Array(s[left + 1...right]) == Array(s[left + 1...right].reversed()){
-            if charDeleted {
-                return false
+        } else {
+            if checkSubsequence(Array(s[left...right - 1])) {
+                return true
             }
 
-            charDeleted = true
+            return checkSubsequence(Array(s[left + 1...right]))
+        }
+    }
+
+    return true
+}
+
+func checkSubsequence(_ s: [Character]) -> Bool {
+
+    var left = 0
+    var right = s.count - 1
+
+    while left < right {
+        if s[left] == s[right] {
             left += 1
-        } else if Array(s[left...right - 1]) == Array(s[left...right - 1].reversed()) {
-            if charDeleted {
-                return false
-            }
-
-            charDeleted = true
             right -= 1
         } else {
             return false
