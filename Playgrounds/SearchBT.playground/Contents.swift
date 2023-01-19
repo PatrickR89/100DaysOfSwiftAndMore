50f64c7539f22b7296d34f002274ed969d7ecf51
PatrickR <patrick@ruzman.net>
2023-01-19 07:47:50 +0100

    LeetCode - 700. Search in a Binary Search Tree
    Runtime 163 ms Beats 77.27%
    Memory 15.1 MB Beats 10.91%

---
+++ Playgrounds/SearchBT.playground/Contents.swift
@@ -0,0 +1,105 @@
+import UIKit
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+public class RootNode {
+    var rootNode: TreeNode?
+
+    init(_ array: [Int?]) {
+        self.rootNode = insertLevel(insertNils(array: array), 0)
+    }
+
+    func insertLevel(_ array: [Int?], _ index: Int) -> TreeNode? {
+
+        var root: TreeNode? = nil
+        if index < array.count {
+            guard let value = array[index] else {
+                return nil
+            }
+            root = TreeNode(value, nil, nil)
+
+            root?.left = insertLevel(array, 2 * index + 1)
+            root?.right = insertLevel(array, 2 * index + 2)
+        }
+        return root
+    }
+
+    func insertNils(array: [Int?]) -> [Int?] {
+        var array = array
+        var tempIndex = [Int]()
+
+        for (index, digit) in array.enumerated() {
+            if digit == nil {
+                if 2 * index + 1 < array.count {
+                    array.insert(nil, at: 2 * index + 1)
+                    array.insert(nil, at: 2 * index + 1)
+                    tempIndex.append(2 * index + 1)
+                    tempIndex.append(2 * index + 2)
+                }
+            }
+        }
+        if !tempIndex.isEmpty {
+            while 2 * tempIndex[0] + 2 < array.count {
+                for index in tempIndex {
+                    if 2*index+2 < array.count {
+                        array.insert(nil, at: 2 * index + 1)
+                        array.insert(nil, at: 2 * index + 1)
+                        tempIndex.append(2 * index + 1)
+                        tempIndex.append(2 * index + 2)
+                        tempIndex.sort()
+                    }
+
+                    let indOfIndex = tempIndex.firstIndex(where: {$0 == index})
+                    tempIndex.remove(at: indOfIndex!)
+                }
+            }
+        }
+        return array
+    }
+}
+
+func searchBST(_ root: TreeNode?, _ val: Int) -> TreeNode? {
+
+    guard let root = root else {
+        return nil
+    }
+
+    var returnNode: TreeNode?
+
+    traverseBT(root, val, &returnNode)
+
+    return returnNode
+}
+
+
+func traverseBT(_ node: TreeNode?, _ val: Int, _ returnNode: inout TreeNode?) {
+    guard let node = node else {
+        return
+    }
+
+    if node.val == val {
+        returnNode = node
+        return
+    }
+
+    traverseBT(node.left, val, &returnNode)
+    traverseBT(node.right, val, &returnNode)
+}
+
+
+let node0 = RootNode([4,2,7,1,3]).rootNode
+let node1 = RootNode([4,2,7,1,3]).rootNode
+
+searchBST(node0, 2)
+searchBST(node1, 5)
