b936b1bf33a08afe2fbd57b0c5dfdd05b133fc17
PatrickR <patrick@ruzman.net>
2023-01-25 10:04:58 +0100

    LeetCode - 1991. Find the Middle Index in Array
    Runtime 10 ms Beats 81.48%
    Memory 14.3 MB Beats 33.33%

--- Playgrounds/FindPivotIndex.playground/Contents.swift
+++ Playgrounds/FindPivotIndex.playground/Contents.swift
@@ -19,6 +19,28 @@ func pivotIndex(_ nums: [Int]) -> Int {
     return -1
 }
 
+/*
+
+func findMiddleIndex(_ nums: [Int]) -> Int {
+    var leftSum = 0
+    var rightSum = nums.reduce(into: 0) { partialResult, val in
+        partialResult = partialResult + val
+    }
+
+    for index in nums.indices {
+        rightSum -= nums[index]
+        if leftSum == rightSum {
+            return index
+        }
+        leftSum += nums[index]
+    }
+
+
+    return -1
+}
+
+*/
+
 pivotIndex([1,7,3,6,5,6])
 pivotIndex([2,1,-1])
 pivotIndex([1,2,3])
