625d00229b6c77430245ddeb7d1a98b4e5625b8f
PatrickR <patrick@ruzman.net>
2023-02-09 08:25:11 +0100

    LeetCode - 917. Reverse Only Letters (w/ help, just learned about .isLetter)
    Runtime 7 ms Beats 55.56%
    Memory 14.5 MB Beats 48.15%

--- Playgrounds/ReverseLettersOnly.playground/Contents.swift
+++ Playgrounds/ReverseLettersOnly.playground/Contents.swift
@@ -1,30 +1,51 @@
 import UIKit
 
 func reverseOnlyLetters(_ s: String) -> String {
+    var characters = [Character]()
+    var s = Array(s)
+    var index = 0
 
-    let pattern = "[a-zA-Z]"
-    var result = Array(repeating: "", count: s.count)
-    let s = Array(s).map { String($0) }
-    var letters = [String]()
-    var counter = s.count
-
-    for index in s.indices {
-        if s[index].range(of: pattern, options: .regularExpression) == nil {
-            result[index] = s[index]
-        } else {
-            letters.insert(s[index], at: 0)
+    s.forEach { char in
+        if char.isLetter {
+            characters.insert(char, at: 0)
         }
     }
 
-    while counter >= 0 {
-        if let index = result.firstIndex(of: "") {
-            result[index] = letters.removeFirst()
+    for charIndex in s.indices {
+        if s[charIndex].isLetter {
+            s[charIndex] = characters[index]
+            index += 1
         }
-        counter -= 1
     }
 
-    return result.joined()
+    return String(s)
 }
+
+//func reverseOnlyLetters(_ s: String) -> String {
+//
+//    let pattern = "[a-zA-Z]"
+//    var result = Array(repeating: "", count: s.count)
+//    let s = Array(s).map { String($0) }
+//    var letters = [String]()
+//    var counter = s.count
+//
+//    for index in s.indices {
+//        if s[index].range(of: pattern, options: .regularExpression) == nil {
+//            result[index] = s[index]
+//        } else {
+//            letters.insert(s[index], at: 0)
+//        }
+//    }
+//
+//    while counter >= 0 {
+//        if let index = result.firstIndex(of: "") {
+//            result[index] = letters.removeFirst()
+//        }
+//        counter -= 1
+//    }
+//
+//    return result.joined()
+//}
 
 reverseOnlyLetters("a-bC-dEf-ghIj")
 //"j-Ih-gfE-dCba"
